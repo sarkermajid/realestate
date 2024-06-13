f85b5d277eb5c8d491a9f5a999d59e93b0c19804
sarkermajid <majidursarkar333@gmail.com>
2024-06-13 10:26:36 +0600

    admin logout setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('admin/login');
     }
 }
--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-        <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="https://easylearningbd.com" target="_blank">EasyLearning</a>.</p>
-        <p class="text-muted">EasyLearning With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
+        <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="https://sarkermajid.github.io/" target="_blank">Sarker Majid</a>.</p>
+        <p class="text-muted">Sarker Majid<i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
 </footer>
